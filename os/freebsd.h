ab4f8d1a36750261c51dc6bbe25651457466506d
Tom Rix <trix@juniper.net>
2014-08-09 12:00:36 -0500

    Add decl's for os thread routines Remove redundant initalization from FRLL

--- os/freebsd.h
+++ os/freebsd.h
@@ -67,5 +67,8 @@
 void ptrace_os_set_singlestep(pid_t pid, long *request);
 void ptrace_os_clear_singlestep(pid_t pid);
 void ptrace_os_option_set_syscall(pid_t pid);
+void ptrace_os_option_set_thread(pid_t pid);
+bool ptrace_os_wait_new_thread(pid_t *out_pid, int *out_status);
+bool ptrace_os_check_new_thread(pid_t pid, int status, pid_t *out_pid);
 
 #endif
