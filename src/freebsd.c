fc4373f7258bb72bbc65761896f556763610c0b1
Tom Rix <trix@juniper.net>
2014-08-11 13:32:28 -0500

    Cleanup checking for new thread function.

--- src/freebsd.c
+++ src/freebsd.c
@@ -61,12 +61,6 @@ void ptrace_os_option_set_syscall(pid_t pid)
 
 void ptrace_os_option_set_thread(pid_t pid)
 {
-#ifdef PT_LWPINFO
-    struct ptrace_lwpinfo lwpinfo = { 0 };
-    if (0 == PTRACE(PT_LWPINFO, pid, &lwpinfo, sizeof(lwpinfo))) {
-	CURRENT_PROCESS_TID = lwpinfo.pl_lwpid;
-    }
-#endif
 }
 
 bool ptrace_os_wait_new_thread(pid_t *out_pid, int *out_status)
@@ -87,13 +81,14 @@ bool ptrace_os_wait_new_thread(pid_t *out_pid, int *out_status)
 
 bool ptrace_os_check_new_thread(pid_t pid, int status, pid_t *out_pid)
 {
+    /* deault to 'not handled' */
     bool ret = false;
-    pid_t ppid = target_get_pid();
-    static bool enter_ret;
 
-    fprintf(stderr, "%x %x\n", pid, ppid);
-
-
+    /*
+     * Set to an invalid pid
+     * This sets up the default 'handled' behaviour to
+     * ignore the event and try again
+     */
     if (out_pid)
 	*out_pid = -1;
 
@@ -118,132 +113,103 @@ bool ptrace_os_check_new_thread(pid_t pid, int status, pid_t *out_pid)
 	    }
 	}
 
-#if 1
-    if (! WIFSTOPPED(status) ||
-	(WSTOPSIG(status) != SIGTRAP)) {
-	fprintf(stderr, "Confused with %d or %d vs %d\n",
-		WIFSTOPPED(status), WSTOPSIG(status), SIGTRAP);
-
-	exit (-1);
-	ret = true;
-	return ret;
-    }
-#endif
-
 	if (lwpinfo.pl_flags & PL_FLAG_SCE) {
-#if 1
-
+	    /* 
+	     * Entering a system call
+	     * Find which system call it was.
+	     */
 	    unsigned long id, arg1, arg2, arg3, arg4, r;
+	    id = -1; /* only initiaze id, because it is the only used variable */
 	    ptrace_arch_get_syscall(&id, &arg1, &arg2, &arg3, &arg4, &r);
-	    fprintf(stderr, "syscall %lx %lx %lx %lx \n", id, arg1, arg2, r);
-#else
-	    unsigned long id;
-	    id = 4;
-#endif
 	    
-
-
-	    if (id == SYS_thr_new) {
-		
-		fprintf(stderr, "thr_new %x %x ------------------- \n", pid, status);
-		ret = true;
-
-	    } else if (id == SYS_thr_exit) {
+	    /* handled */
+	    ret = true;
+	    
+	    if (id == SYS_thr_exit) {
+	    /*
+	     * On the edge of exiting the thread
+	     * Reset the global current thread to the parent process
+	     * Then manually continue with the dieing thread
+	     */
 		target_dead_thread(pid);
-		_target.current_process = 0; /* TODO rework */
+		_target.current_process = 0; /* parent process index */
 		if (out_pid)
 		    *out_pid = CURRENT_PROCESS_TID;
-
+		
 		PTRACE(PT_CONTINUE, pid, 1, 0);
 		pid = waitpid(-1, &status, 0);
 		
-		fprintf(stderr, "thr_exit %x %x ------------------------\n", pid, status);
-		ret = true;
-	    } else if (id == SYS_exit) {
-//		ret = false;
-		ret = true;
-	    } else {
-		ret = true;
 	    }
-	    enter_ret = ret;
-	} else if (lwpinfo.pl_flags & PL_FLAG_SCX) {
 
-	    ret = enter_ret;
-#if 1
+	} else if (lwpinfo.pl_flags & PL_FLAG_SCX) {
+	    
 	    int num_lwps = 0;
 	    int num_threads = 0;
 	    lwpid_t *lwpid_list = NULL;
 	    
 	    /* Handled but invalid */
 	    ret = true;
-	    if (out_pid)
-		*out_pid = -1;
-
+	    
 	    num_lwps = PTRACE(PT_GETNUMLWPS, pid, NULL, 0);
 	    num_threads = target_number_threads();
-    
+	    
+	    /*
+	     * Look for different in number of threads the system
+	     * has versus what we have.
+	     *
+	     * WARNING : System does not seem to report dead threads
+	     * by removing the number of threads reported by PT_GETNUMLWPS
+	     * This means neither can we.
+	     */
 	    if (num_lwps != num_threads) {
-
-		fprintf(stderr, "%d vs %d\n", num_lwps, num_threads);
-
 		if (num_lwps) {
-		    fprintf(stderr, "%d \n", __LINE__);
-
 		    lwpid_list = (lwpid_t *) calloc(num_lwps, sizeof(lwpid_t));
 		    if (lwpid_list) {
-			fprintf(stderr, "%d \n", __LINE__);
-
 			if (num_lwps == PTRACE(PT_GETLWPLIST, pid, lwpid_list, num_lwps)) {
-			    int i;
-			    fprintf(stderr, "%d %d %d\n", __LINE__, num_lwps, num_threads);
+			    /* More than expected, A new thread is born! */
 			    if (num_lwps > num_threads) {
-
-				fprintf(stderr, "%d \n", __LINE__);
-
 				pid_t parent = target_get_pid();
-				/* A thread is born */
+				int i;
 				for (i = 0; i < num_lwps; i++) {
+				    /* Find the one that isn't already being tracked */
 				    if (! target_is_tid(lwpid_list[i])) {
 					if (target_new_thread(parent, lwpid_list[i])) {
 					    if (out_pid)
 						*out_pid = CURRENT_PROCESS_TID;
-					    ret = true;
 					}
 					break;
 				    }
 				}
 			    } else {
-				fprintf(stderr, "%d \n", __LINE__);
-
-				fprintf(stderr, "A thread has died\n");
-				/* A thread has died */
+				/*
+				 * A thread has died
+				 *
+				 * In development, this case was never reached.
+				 * Leave it here in case somthing improved
+				 */
+				DBG_PRINT("Unexpected code hit %s %d \n", __func__, __LINE__);
 			    }
 			} else {
-			    fprintf(stderr, "%d \n", __LINE__);
-
 			    DBG_PRINT("Error with PT_GETLWPLIST\n");
 			}
 		    } else {
-			fprintf(stderr, "%d \n", __LINE__);
-
 			DBG_PRINT("Error allocating lwpid_list\n");
 		    }
 		} else {
-
-		    fprintf(stderr, "%d \n", __LINE__);
-
 		    /* Everyone is dead */
 		    target_all_dead_thread();
 		}
 	    }
-
+	    
 	    if (lwpid_list)
 		free(lwpid_list);
-#endif
+	    
 	} else {
-
-	    fprintf(stderr, "UNHANDLED lwpinfo.pl_flags %x \n", lwpinfo.pl_flags);
-
+	    
+	    if (_lwpinfo_verbose) {
+		DBG_PRINT("UNHANDLED lwpinfo.pl_flags %x \n", lwpinfo.pl_flags);
+	    }
+	    
 	    /* Stopping for something that isn't a system call, like a breakpoint */
 	    if (lwpinfo.pl_flags & PL_FLAG_SI) {
 		ret = false;
@@ -251,14 +217,11 @@ bool ptrace_os_check_new_thread(pid_t pid, int status, pid_t *out_pid)
 		/* Maybe ? */
 		ret = true;
 	    }
-
+	    
 	} /* sycall exit check */
 	
     } /* pt_lwpinfo */
-
 #endif
-
-    fprintf(stderr, "ret %d\n", ret);
 
     return ret;
 }
