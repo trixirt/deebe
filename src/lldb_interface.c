da806d84161c1da838f0c1c4c318671ad536a918
Tom Rix <trix@juniper.net>
2015-12-13 10:12:22 -0800

    Improve handling of qProcessInfo packet Hex encode the triple strings

--- src/lldb_interface.c
+++ src/lldb_interface.c
@@ -43,6 +43,7 @@
 #include "gdb_interface.h"
 #include "lldb_interface.h"
 #include "target.h"
+#include "util.h"
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 static char *endian_str = "little";
@@ -59,14 +60,14 @@ static bool get_triple(char **ptr) {
      * How big will the triple string be..
      * element are null terminated, 3 strlens
      * need a null, and 2 '-' .. + 3
-     * need an 'unknown' vendor + 7
+     * need an 'unknown' vendor + 7 -- xxx may not linux doesn't use it
      * maybe need a prefix '-gnu' or '-gnueabi' + 7
      * so 17 known/maybe + 2 strlens.. , round 17 up to 32
      */
     *ptr = (char *) malloc (32 + strlen(&name.sysname[0]) + strlen(&name.machine[0]));
     if (*ptr != NULL) {
       if (strncmp(&name.sysname[0], "Linux", 5) == 0)
-	sprintf(*ptr, "%s-unknown-linux-gnu", &name.machine[0]);
+	sprintf(*ptr, "%s--linux-gnu", &name.machine[0]);
       else if (strncmp(&name.sysname[0], "FreeBSD", 7) == 0)
 	sprintf(*ptr, "%s-unknown-freebsd", &name.machine[0]);
       else
@@ -100,13 +101,25 @@ bool lldb_handle_query_command(char * const in_buf, int in_len, char *out_buf, i
   char *n = in_buf + 1;
   bool req_handled = false;
   char *triple_str = NULL;
+  char *encoded_triple_str = NULL;
   char *ostype_str = NULL;
+  size_t encoded_triple_str_size = 0;
 
   switch (*n) {
   case 'H':
     if (strncmp(n, "HostInfo", 8) == 0) {
       if (get_triple(&triple_str)) {
-	snprintf(out_buf, out_buf_len, "triple:%s;ptrsize:%u;endian:%s", triple_str, (unsigned) sizeof(void *), endian_str);
+	encoded_triple_str_size = 1 + 2 * strlen(triple_str);
+	encoded_triple_str = (char *) malloc (encoded_triple_str_size);
+	if (encoded_triple_str != NULL) {
+	  /* Assume the encoding doesn't fail.. */
+	  util_encode_string(triple_str, encoded_triple_str, encoded_triple_str_size);
+	  snprintf(out_buf, out_buf_len, "triple:%s;ptrsize:%u;endian:%s", encoded_triple_str, (unsigned) sizeof(void *), endian_str);
+	  free(encoded_triple_str);
+	  encoded_triple_str = NULL;
+	} else {
+	  gdb_interface_write_retval(RET_ERR, out_buf);
+	}
 	free(triple_str);
 	triple_str = NULL;
       } else {
@@ -136,17 +149,28 @@ bool lldb_handle_query_command(char * const in_buf, int in_len, char *out_buf, i
       goto end;
     } else if (strncmp(n, "ProcessInfo", 11) == 0) {
       if (get_triple(&triple_str)) {
-	if (get_ostype(&ostype_str)) {
-	  /* Not supporting multi process so ever process is created by deebe */
-	  pid_t my_pid = getpid();
-	  /* Keep it simple and use deebe's uid/euid till it breaks */
-	  uid_t my_uid = getuid();
-	  uid_t my_euid = geteuid();
-	  gid_t my_gid = getgid();
-	  gid_t my_egid = getgid();
-	  snprintf(out_buf, out_buf_len, "pid:%x;parent-pid:%x;real-uid:%x;real-guid:%x;effective-uid:%x;effective-gid:%x;triple:%s;ostype:%s;endian:%s;ptrsize:%u", CURRENT_PROCESS_PID, my_pid, my_uid, my_gid, my_euid, my_egid, triple_str, ostype_str, endian_str, (unsigned) sizeof(void *));
-	  free(ostype_str);
-	  ostype_str = NULL;
+	encoded_triple_str_size = 1 + 2 * strlen(triple_str);
+	encoded_triple_str = (char *) malloc (encoded_triple_str_size);
+	if (encoded_triple_str != NULL) {
+	  /* Assume the encoding doesn't fail.. */
+	  util_encode_string(triple_str, encoded_triple_str, encoded_triple_str_size);
+
+	  if (get_ostype(&ostype_str)) {
+	    /* Not supporting multi process so ever process is created by deebe */
+	    pid_t my_pid = getpid();
+	    /* Keep it simple and use deebe's uid/euid till it breaks */
+	    uid_t my_uid = getuid();
+	    uid_t my_euid = geteuid();
+	    gid_t my_gid = getgid();
+	    gid_t my_egid = getgid();
+	    snprintf(out_buf, out_buf_len, "pid:%x;parent-pid:%x;real-uid:%x;real-guid:%x;effective-uid:%x;effective-gid:%x;triple:%s;ostype:%s;endian:%s;ptrsize:%u;", CURRENT_PROCESS_PID, my_pid, my_uid, my_gid, my_euid, my_egid, encoded_triple_str, ostype_str, endian_str, (unsigned) sizeof(void *));
+	    free(ostype_str);
+	    ostype_str = NULL;
+	  } else {
+	    gdb_interface_write_retval(RET_ERR, out_buf);
+	  }
+	  free(encoded_triple_str);
+	  encoded_triple_str = NULL;
 	} else {
 	  gdb_interface_write_retval(RET_ERR, out_buf);
 	}
